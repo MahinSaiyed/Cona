5d1e76887256dbcc8673b9060f9af32716d2bbf6
MahinSaiyed <mahinsaiyed417@gmail.com>
2025-07-04 13:50:17 +0530

    update  php function

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body style="background-color: black">
+  <p style="color: white;  font-size : 20px;">
+    team work with saiyed aadil and ali
+    i'm team leader for project .
+  </p>
+
+  <?php
+  function myFamily($lastname, ...$firstname)
+  {
+    $txt = "";
+    $len = count($firstname);
+    for ($i = 0; $i < $len; $i++) {
+      $txt = $txt . "Hi, $firstname[$i] $lastname.<br>";
+    }
+    return $txt;
+  }
+
+  $a = myFamily("Doe", "Mahin", "Aadil", "Ali");
+  echo $a;
+  
+  # Hi, Mahin Doe.
+  # Hi, Aadil Doe.
+  # Hi, Ali Doe. 
+  ?>
+</body>
+</html>
