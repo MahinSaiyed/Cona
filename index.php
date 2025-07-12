a8f1a46a58955ac634465a806c9b0c6a2dfd3477
MahinSaiyed <mahinsaiyed417@gmail.com>
2025-07-12 23:45:20 +0530

    timing

--- index.php
+++ index.php
@@ -13,23 +13,8 @@
   </p>
 
   <?php
-  function myFamily($lastname, ...$firstname)
-  {
-    $txt = "";
-    $len = count($firstname);
-    for ($i = 0; $i < $len; $i++) {
-      $txt = $txt . "Hi, $firstname[$i] $lastname.<br>";
-    }
-    return $txt;
-  }
-
-  $a = myFamily("Doe", "Mahin", "Aadil", "Ali");
-  echo $a;
-  
-  # Output -1
-  # Hi, Mahin Doe.
-  # Hi, Aadil Doe.
-  # Hi, Ali Doe. 
+  echo "The time is " . date("h:i:sa");
   ?>
+
 </body>
 </html>
